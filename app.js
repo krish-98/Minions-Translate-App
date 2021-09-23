a08a3f863bc0cb1b004ad0d2213c472225682685
krish-98 <muralikrishnanm98@gmail.com>
2021-09-23 14:07:28 +0530

    test: wiring js with html

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const btn = document.querySelector("#btn")
+const inputTxt = document.querySelector("#input-txt")
+const btn = document.querySelector("#btn-translate")
+const outputTxt = document.querySelector("#output-txt")
 
-btn.addEventListener("click", () => console.log("Button was clicked!"))
+
+btn.addEventListener("click", () => {
+  console.log("button was clicked", inputTxt, btn, outputTxt)
+})
