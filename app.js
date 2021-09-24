1bc1933e8f1295b85a553476ecf6beecbc03bab5
krish-98 <muralikrishnanm98@gmail.com>
2021-09-24 13:19:09 +0530

    translation api is working

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const inputTxt = document.querySelector("#input-txt")
 const btn = document.querySelector("#btn-translate")
 const outputTxt = document.querySelector("#output-txt")
 
+const serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-btn.addEventListener("click", () => {
-  console.log("button was clicked", inputTxt, btn, outputTxt)
-})
+const constructURL = text => serverURL + "?text=" + text
+
+const eventHandler = () => {
+  const userInput = inputTxt.value
+
+  fetch(constructURL(userInput))
+  .then(res => res.json())
+  .then(data => outputTxt.innerText = data.contents.translated)
+}
+
+btn.addEventListener("click", eventHandler)
